a41dff8cf2a7525314b7005951121c3f9a743cc4
sreeramraghu04 <sreeramraghu04@gmail.com>
2025-04-30 12:47:38 +0530

    seventh

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,41 +1,3 @@
-/* import PropTypes from "prop-types"; */
-/* import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import EditDocumentIcon from "@mui/icons-material/EditDocument";
-
-const BlogPost = ({ blog, deleteBlog, editBlog }) => {
-  return (
-    <div className="max-w-md bg-gray-200 rounded-xl shadow-md p-4 flex flex-col justify-center items-end mt-10">
-      <img
-        src={blog.image}
-        alt={`Image for ${blog.title}`}
-        className="w-full h-64 object-cover rounded-xl"
-      />
-      <div>
-        <div className="p-2">
-          <h2 className="text-2xl underline font-bold mb-2">{blog.title}</h2>
-          <p className="text-gray-700">{blog.description}</p>
-          <div className="flex flex-row gap-5">
-            <p className="text-sm text-gray-500 mt-8 w-[300px]">
-              {blog.date} | {blog.day}
-            </p>
-            <DeleteForeverIcon
-              className="text-red-600 cursor-pointer mt-8 opacity-75 hover:opacity-100"
-              onClick={() => deleteBlog(blog.id)}
-            />
-            <EditDocumentIcon
-              className="text-black cursor-pointer mt-8 opacity-75 hover:opacity-100"
-              onClick={() => editBlog(blog.id)}
-            />
-          </div>
-          <div className="space-x-2"></div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default BlogPost; */
-
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditDocumentIcon from "@mui/icons-material/EditDocument";
 
@@ -47,26 +9,23 @@ const BlogPost = ({ blog, deleteBlog, editBlog }) => {
         alt={`Image for ${blog.title}`}
         className="w-full h-64 object-cover rounded-xl"
       />
-      <div className="w-full">
-        <div className="p-4">
-          <h2 className="text-2xl underline font-bold mb-2">{blog.title}</h2>
-          <p className="text-gray-700">{blog.description}</p>
-          <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5 mt-6">
-            <p className="text-sm text-gray-500 w-full sm:w-[300px]">
-              {blog.date} | {blog.day}
-            </p>
-            <div className="flex gap-3">
-              <DeleteForeverIcon
-                className="text-red-600 cursor-pointer opacity-75 hover:opacity-100"
-                onClick={() => deleteBlog(blog.id)}
-              />
-              <EditDocumentIcon
-                className="text-black cursor-pointer opacity-75 hover:opacity-100"
-                onClick={() => editBlog(blog.id)}
-              />
-            </div>
+      <div className="p-2 w-full">
+        <h2 className="text-2xl underline font-bold mb-2">{blog.title}</h2>
+        <p className="text-gray-700">{blog.description}</p>
+        <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5 mt-6">
+          <p className="text-sm text-gray-500 w-full sm:w-[300px]">
+            {blog.date} | {blog.day}
+          </p>
+          <div className="flex gap-3">
+            <DeleteForeverIcon
+              className="text-red-600 cursor-pointer opacity-75 hover:opacity-100"
+              onClick={() => deleteBlog(blog.id)}
+            />
+            <EditDocumentIcon
+              className="text-black cursor-pointer opacity-75 hover:opacity-100"
+              onClick={() => editBlog(blog.id)}
+            />
           </div>
-          <div className="space-x-2"></div>
         </div>
       </div>
     </div>
@@ -74,3 +33,5 @@ const BlogPost = ({ blog, deleteBlog, editBlog }) => {
 };
 
 export default BlogPost;
+
+
