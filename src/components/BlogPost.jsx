fb6d4d092d6052d89809c5fe303a5c00b8572c91
sreeramraghu04 <sreeramraghu04@gmail.com>
2025-04-30 12:44:54 +0530

    sixeth

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,5 +1,5 @@
 /* import PropTypes from "prop-types"; */
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+/* import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditDocumentIcon from "@mui/icons-material/EditDocument";
 
 const BlogPost = ({ blog, deleteBlog, editBlog }) => {
@@ -11,7 +11,7 @@ const BlogPost = ({ blog, deleteBlog, editBlog }) => {
         className="w-full h-64 object-cover rounded-xl"
       />
       <div>
-        <div className="p-">
+        <div className="p-2">
           <h2 className="text-2xl underline font-bold mb-2">{blog.title}</h2>
           <p className="text-gray-700">{blog.description}</p>
           <div className="flex flex-row gap-5">
@@ -27,9 +27,46 @@ const BlogPost = ({ blog, deleteBlog, editBlog }) => {
               onClick={() => editBlog(blog.id)}
             />
           </div>
-          <div className="space-x-2">
-            
+          <div className="space-x-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost; */
+
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditDocumentIcon from "@mui/icons-material/EditDocument";
+
+const BlogPost = ({ blog, deleteBlog, editBlog }) => {
+  return (
+    <div className="w-full max-w-md bg-gray-200 rounded-xl shadow-md p-4 flex flex-col justify-center items-end mt-10 mx-auto">
+      <img
+        src={blog.image}
+        alt={`Image for ${blog.title}`}
+        className="w-full h-64 object-cover rounded-xl"
+      />
+      <div className="w-full">
+        <div className="p-4">
+          <h2 className="text-2xl underline font-bold mb-2">{blog.title}</h2>
+          <p className="text-gray-700">{blog.description}</p>
+          <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5 mt-6">
+            <p className="text-sm text-gray-500 w-full sm:w-[300px]">
+              {blog.date} | {blog.day}
+            </p>
+            <div className="flex gap-3">
+              <DeleteForeverIcon
+                className="text-red-600 cursor-pointer opacity-75 hover:opacity-100"
+                onClick={() => deleteBlog(blog.id)}
+              />
+              <EditDocumentIcon
+                className="text-black cursor-pointer opacity-75 hover:opacity-100"
+                onClick={() => editBlog(blog.id)}
+              />
+            </div>
           </div>
+          <div className="space-x-2"></div>
         </div>
       </div>
     </div>
